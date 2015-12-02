86ed77c7859bcf4670f4f5e17f0b2e077fe12138
jreddington76 <jreddington76@users.noreply.github.com>
2015-12-02 14:53:53 +0000

    Added unit tests

---
+++ IsaacAva.UI/app/collection/collection.specs.js
@@ -0,0 +1,38 @@
+﻿describe('collection', function () {
+
+	beforeEach(module('app'));
+
+	var $controller;
+	var collectionService;
+
+	beforeEach(inject(function (_$controller_) {
+		$controller = _$controller_;
+		//controller = $controller('CollectionController', { collectionService: collectionService });
+	}));
+
+	describe('controller', function () {
+		it('should not be null', function () {
+			expect(controller).not.toBe(null);
+		});
+	});
+
+	describe('slides', function () {
+		it('should contain 6 elements', function () {
+			collectionService = { getData: function () { } };
+			var mock = sinon.mock(collectionService);
+			mock.expects("getData")
+			var collection = controller.collection;
+			expect(collection.length).toEqual(6);
+		});
+	});
+
+	// create a spy and verify it was called once
+	describe('collectionService', function () {
+		it('getData was called once', function () {
+			var spy = sinon.spy();
+			var controller = $controller('CollectionController', { collectionService: spy });
+			var collection = controller.collection;
+			expect(spy.called).toEqual(true);
+		});
+	});
+});
