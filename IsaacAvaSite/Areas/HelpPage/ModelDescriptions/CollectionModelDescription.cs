23ba26d3ab6c9e5256514da05a3ab43a18561f5e
jreddington76 <jreddington76@users.noreply.github.com>
2015-12-04 12:42:25 +0000

    Removed obsolete project IsaacAvaSite

--- IsaacAvaSite/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace IsaacAvaSite.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- IsaacAvaSite/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace IsaacAvaSite.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
