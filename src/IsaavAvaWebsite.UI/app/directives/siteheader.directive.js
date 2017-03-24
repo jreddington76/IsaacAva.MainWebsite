6f48c6344cb01db6119776ab5e02fba4d7b3cad8
reddingja <reddingja@W7LAPT1057>
2017-03-24 09:49:42 +0000

    further styling

---
+++ src/IsaavAvaWebsite.UI/app/directives/siteheader.directive.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular
+		.module("app")
+		.directive('iaSiteHeader', siteHeader);
+
+    function siteHeader() {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/assets/templates/header.html'
+        };
+    };
+})();
