d2b3425251fae72025a7a52b28f72fc3c7cbbb0f
reddingja <reddingja@W7LAPT1057>
2017-03-27 08:58:18 +0100

    Added thumbnail directive

---
+++ src/IsaavAvaWebsite.UI/app/directives/thumbnail.directive.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    angular
+		.module("app")
+		.directive('iaThumbnail', thumbnail);
+
+    function thumbnail() {
+        return {
+            restrict: 'E',
+            controller: 'ThumbnailController',
+            controllerAs: 'vm1',
+            templateUrl: 'app/thumbnails/thumbnail.html'
+        };
+    };
+})();
